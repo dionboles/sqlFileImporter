30b362b0f8ce9a200c6ef8d53d552b15cfe3ff67
Dion Boles <dion.boles@gmail.com>
2019-04-11 13:59:11 -0400

    Update sqlImport.py

--- sqlImport.py
+++ sqlImport.py
@@ -1,7 +1,7 @@
 import mysql.connector
 from time import sleep
 cnx = mysql.connector.connect(user='****', password='***',
-                              port = ****,
+                              port = 3306
                               host='127.0.0.1',
                               database='populate')
 cursor = cnx.cursor()
